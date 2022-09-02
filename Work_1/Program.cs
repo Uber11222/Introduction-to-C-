c1b461dd732d0b2afc788ea06cc9f789fdcc56dd
IRazumov <ivpostraz@yandex.ru>
2022-09-02 20:02:16 +0400

    Закончил первую задачу, надо вторую

--- Work_1/Program.cs
+++ Work_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+string number1 = Console.ReadLine();
+int a = Convert.ToInt32(number1);
+Console.WriteLine("Введите второе число");
+string number2 = Console.ReadLine();
+int b = Convert.ToInt32(number2);
+if (a>b) {
+    Console.WriteLine("Большее {0}, меньшее {1}", a, b);
+}
+else {
+    Console.WriteLine("Большее {1}, меньшее {0}", a, b);
+}
