30bb64b5ccdc1bc1976081c14d0cfddef2e65a0e
IRazumov <ivpostraz@yandex.ru>
2022-09-02 20:59:03 +0400

    Сделал 3 задачу

---
+++ Work_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int a = Convert.ToInt32(number);
+if (a%2 == 0) {
+   Console.WriteLine("Чётное ли число {0}: да", a);
+}
+else {
+   Console.WriteLine("Чётное ли число {0}: нет", a);
+}
